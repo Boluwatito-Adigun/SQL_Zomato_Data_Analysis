c10681709e9b387d28a0faed692d51d1d3173fc9
Boluwatito Samuel <samuelgabby04@gmail.com>
2025-12-03 21:17:29 -0300

    Commit

---
+++ project_scripts/Business_problems_analysis.sql
@@ -0,0 +1,15 @@
+/*Q1. Top 5 Most Frequently Ordered Dishes
+Question:
+Write a query to find the top 5 most frequently ordered dishes by the customer "Arjun Mehta" in
+the last 1 year. */
+
+
+/*Q2. Popular Time Slots
+Question:
+Identify the time slots during which the most orders are placed, based on 2-hour intervals. */
+
+
+/*Q3. Order Value Analysis
+Question:
+Find the average order value (AOV) per customer who has placed more than 750 orders.
+Return: customer_name, aov (average order value).*/
